53fe6d41d52b31c61709ee00a7541af06ed4de94
LiveTembiso <livetembiso0@gmail.com>
2019-07-25 16:01:06 +0100

    Added a button

--- prac1.py
+++ prac1.py
@@ -12,13 +12,18 @@ from gpiozero import LED
 from time import sleep
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7,GPIO.OUT)
+GPIO.setup(11,GPIO.IN,pull_up_down = GPIO.PUD_UP)
 
 def main():
-	print("Hello world")
-	GPIO.output(7,True)
-	sleep(2)
-	GPIO.output(7,False)
-	sleep(2)
+	button_state = GPIO.input(23)
+	if button_state == False:
+		print("Hello world")
+		GPIO.output(7,True)
+		sleep(2)
+		GPIO.output(7,False)
+		sleep(2)
+	else:
+		GPIO.output(24,False)
 
 if __name__ == "__main__":
 	#GPIO stopped correctly
