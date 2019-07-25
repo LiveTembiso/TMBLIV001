a80bfa84348ea347607fb2093b8c118ca48e93d2
LiveTembiso <livetembiso0@gmail.com>
2019-07-25 20:05:37 +0100

    Have 2 buttons and 1 LED

--- prac1.py
+++ prac1.py
@@ -13,28 +13,44 @@ from time import sleep
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7,GPIO.OUT)
 GPIO.setup(11,GPIO.IN,pull_up_down = GPIO.PUD_UP)
+GPIO.setup(12,GPIO.IN,pull_up_down = GPIO.PUD_UP)
+counter = 0
 
-def main():
-	button_state = GPIO.input(23)
-	if button_state == False:
-		print("Hello world")
-		GPIO.output(7,True)
-		sleep(2)
-		GPIO.output(7,False)
-		sleep(2)
-	else:
-		GPIO.output(24,False)
+#Increases counter when button(pin 11) is pressed
+def increment(channel):
+	print("Increment")
+	if counter < 7:
+		counter += 1
+	elif counter == 7:
+		counter = 0
+	GPIO.output(7,True)
+	sleep(1)
 
-if __name__ == "__main__":
+#Decreases couunter when button(pin 12) is pressed
+def decrement(channel):
+	print("Decrement")
+	if counter > 0:
+		counter -= 1
+	elif counter == 0:
+		counter = 7
+	GPIO.output(7,True)
+	sleep(1)
+
+GPIO.add_event_detect(11,GPIO.FALLING,callback=increment,bouncetime=300)
+GPIO.add_event_detect(12,GPIO.FALLING,callback=decrement,bouncetime=300)
+
+#if __name__ == "__main__":
 	#GPIO stopped correctly
-	try:
-		while True:
-			main()
-	except KeyboardInterrupt:
-		print("Exiting")
-		#Turn off GPIOs
-		GPIO.cleanup()
-	except e:
-		GPIO.cleanup()
-		print("Some other error occured")
-		print(e.message)
+try:
+	while True:
+		#main()
+		print("Counter = ", counter)
+		sleep(1)
+except KeyboardInterrupt:
+	print("Exiting")
+	#Turn off GPIOs
+	GPIO.cleanup()
+except e:
+	GPIO.cleanup()
+	print("Some other error occured")
+	print(e.message)
