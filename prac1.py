417155b0d9c0b32c020641eaadc2737ec762ce38
LiveTembiso <livetembiso0@gmail.com>
2019-07-25 01:45:12 +0100

    Test1

--- prac1.py
+++ prac1.py
@@ -8,18 +8,25 @@ Date: 25th July 2019
 """
 
 import RPi.GPIO as GPIO
+from time import sleep
+from gpiozero import LED
 
+led = LED(7)
 
 def main():
 	print("Hello world")
+	led.on()
+	sleep(2)
+	led.off()
+	sleep(2)
 
-if __name__== "__main__":
+if __name__ == "__main__":
 	#GPIO stopped correctly
 	try:
 		while True:
 			main()
 	except KeyboardInterrupt:
-		print("Exitting")
+		print("Exiting")
 		#Turn off GPIOs
 		GPIO.cleanup()
 	except e:
